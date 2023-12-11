743388d46670241e20de1c51ea874f632cceae77
Nguyễn Hoàng Huy <105409022+hoanghuy27th8@users.noreply.github.com>
2023-12-11 15:10:35 +0700

    Update app.py

--- GT/app.py
+++ GT/app.py
@@ -9,16 +9,16 @@ CORS(app)
 data = pd.read_csv('MauDataGoiY.csv')
 X = data.drop('goi_y', axis=1)
 y = data['goi_y']
-clf = DecisionTreeClassifier(criterion='gini')
+clf = DecisionTreeClassifier(criterion='entropy')
 clf.fit(X, y)
 
 @app.route("/", methods=['POST'])
 def test():
-    test_data = request.get_json()
-    data_input = test_data.get('data', [])
-    new = pd.DataFrame([data_input], columns=X.columns) 
-    y_pred = clf.predict(new)
-    return jsonify({'ket_qua': y_pred.tolist()})  
+    test = request.get_json()
+    dataInput = test_data.get('data', [])
+    new = pd.DataFrame([dataInput], columns=X.columns) 
+    ketQua = clf.predict(new)
+    return jsonify({'ket_qua': ketQua.tolist()})  
 
 if __name__ == '__main__':
     app.run(debug=True)
